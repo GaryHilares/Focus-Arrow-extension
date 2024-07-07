4d274cfd6843916154dd01fbc6509a200a2e02fb
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-07 13:01:45 -0500

    Added CSS reset and global font family

--- src/settings/index.tsx
+++ src/settings/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./pages/App";
+import "./globals.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
