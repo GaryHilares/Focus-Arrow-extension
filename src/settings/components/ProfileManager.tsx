5ecfbaadc9197c27b7f3afd12294c6281df42b0f
Gary Hilares <dev.garyhilares@gmail.com>
2024-07-08 13:01:57 -0500

    Improved presentation for ProfileManager

--- src/settings/components/ProfileManager.tsx
+++ src/settings/components/ProfileManager.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as styles from "./ProfileManager.module.scss";
 
 export type Pattern = { name: string; url: string };
 
@@ -14,22 +15,34 @@ function ProfileManager({
   ) => void;
 }) {
   return (
-    <div>
-      <button onClick={() => setModalMode({ mode: "new" })}>Add</button>
+    <section className={styles.main}>
+      <header>
+        <h1>Pages</h1>
+        <button onClick={() => setModalMode({ mode: "new" })}>Add</button>
+      </header>
       <ul>
         {patterns.map((pattern) => (
-          <li key={pattern.name}>
-            {pattern.name}
-            <button
-              onClick={() => setModalMode({ mode: "edit", target: pattern })}
-            >
-              Edit
-            </button>
-            <button onClick={() => deletePattern(pattern)}>Delete</button>
+          <li key={pattern.name} className={styles.entry}>
+            <span className={styles["entry-name"]}>{pattern.name}</span>
+            <span className={styles["entry-url"]}>{pattern.url}</span>
+            <ul>
+              <li>
+                <button
+                  onClick={() =>
+                    setModalMode({ mode: "edit", target: pattern })
+                  }
+                >
+                  Edit
+                </button>
+              </li>
+              <li>
+                <button onClick={() => deletePattern(pattern)}>Delete</button>
+              </li>
+            </ul>
           </li>
         ))}
       </ul>
-    </div>
+    </section>
   );
 }
 
